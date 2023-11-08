3de878adaeb2b15cdd979c7d9664cbb944c351c6
Daffa-code1 <148167874+Daffa-code1@users.noreply.github.com>
2023-11-08 19:44:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function calculateBMI() {
+    var weightInput = document.getElementById("weight-bar");
+    var heightInput = document.getElementById("height-bar");
+    var resultDiv = document.getElementById("hasil");
+  
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+  
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Silahkan masukkan tinggi dan berat badan yang valid.";
+      return;
+    }
+   
+    var bmi = weight / ((height / 100) ** 2);
+    var category = "";
+  
+    if (bmi < 18.5) {
+      category = "Berat badan kurang";
+    } else if (bmi < 25) {
+      category = "Berat badan ideal/normal";
+    } else if (bmi < 30) {
+      category = "Kelebihan berat badan";
+    } else {
+      category = "Kegemukan [Obesitas]";
+    }
+  
+    resultDiv.innerHTML = "BMI kamu adalah " + bmi.toFixed(2) + " (" + category + ")";
+  }
